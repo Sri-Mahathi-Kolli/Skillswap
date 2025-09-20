53b6ec2ee48d011fc90d2657990e8df42f446f1a
Sri <sri17mahathi@gmail.com>
2025-09-19 21:49:16 -0400

    render1

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api',
-  socketUrl: 'http://localhost:3000',
+  apiUrl: 'https://medhikaa.onrender.com/api',
+  socketUrl: 'https://medhikaa.onrender.com',
   stripePublishableKey: 'pk_test_51S2M7pBiOdK8gYs8ynQUFp3zDcwxtTFeeFJguXFwiTJixcEBSS7O7hccLXzNeSQnelTQL2YOS4TfrJgkj3XWQadi0015BKbYKr',
   zoomClientId: 'your_zoom_client_id',
   appName: 'SkillSwap',
